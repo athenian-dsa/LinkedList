786d2b37da0193b4d3eeaf1cb043ca0f4b81e53a
Anay Pant <anaypant212@gmail.com>
2023-12-19 21:12:34 -0800

    mytests

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,5 +1,3 @@
-package test.java;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +5,70 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MyTests {
     @Test
     public void testInsertFirst() {
-        assertTrue(false);
+
+        IntLinkedList l1 = new IntLinkedList();
+        int[] li = new int[]{3, 1, 6};
+        for(int i: li){
+            l1.insertFirst(i);
+        }
+        assertTrue(l1.search(3) && l1.search(1) && l1.search(6));
+    }
+
+    @Test
+    public void testInsertLast() {
+
+        IntLinkedList l1 = new IntLinkedList();
+        int[] li = new int[]{3, 1, 6};
+        for(int i: li){
+            l1.insertLast(i);
+        }
+        assertTrue(l1.search(3) && l1.search(1) && l1.search(6));
+    }
+
+    @Test
+    public void testSearch() {
+
+        IntLinkedList l1 = new IntLinkedList();
+        int[] li = new int[]{3, 1, 6};
+        for(int i: li){
+            l1.insertLast(i);
+        }
+        assertTrue(l1.search(3) && l1.search(1) && l1.search(6) && !l1.search(5));
+    }
+
+    @Test
+    public void testRemoveFirst() {
+
+        IntLinkedList l1 = new IntLinkedList();
+        int[] li = new int[]{3, 1, 6};
+        for(int i: li){
+            l1.insertLast(i);
+        }
+        assertTrue(l1.removeFirst()==3 && l1.removeFirst()==1 && l1.removeFirst()==6);
+    }
+
+    @Test
+    public void testRemoveLast() {
+
+        IntLinkedList l1 = new IntLinkedList();
+        int[] li = new int[]{3, 1, 6};
+        for(int i: li){
+            l1.insertLast(i);
+        }
+        assertTrue(l1.removeFirst()==6 && l1.removeFirst()==1 && l1.removeFirst()==3);
+    }
+
+    @Test
+    public void testRemoveAll() {
+
+        IntLinkedList l1 = new IntLinkedList();
+        int[] li = new int[]{3, 1, 6, 1, 3, 5};
+        for(int i: li){
+            l1.insertLast(i);
+        }
+        l1.removeAll(1);
+        assertFalse(l1.search(1));
+        l1.removeAll(3);
+        assertFalse(l1.search(3));
     }
 }
